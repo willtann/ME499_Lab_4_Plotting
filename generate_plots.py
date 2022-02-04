6ecd513c3ef1440cf0740d4cd410ce702a104db2
Tanner WIlliams <97181243+willtann@users.noreply.github.com>
2022-02-04 09:58:15 -0800

    Friday 01.04.2022

--- generate_plots.py
+++ generate_plots.py
@@ -3,7 +3,7 @@ from msd import MassSpringDamper
 import matplotlib.pyplot as plt
 
 
-""" Initial values and inputs of system for Problem 1 """
+""" Initial values of system for Problem 1: Spring Damper System """
 mass = 1.0  # mass
 spring = 5.0  # spring constant
 damper = 2.5  # damping ratio
@@ -12,15 +12,18 @@ dt_in = 0.01  # time step size
 x_in = 1  # initial position
 x_dot_in = -1  # initial velocity
 
+# Using class MassSpringDamper from msd.py to model position and velocity over time with the inputs
 smd = MassSpringDamper(mass, spring, damper)
 state, t = smd.simulate(x_in, x_dot_in, t_in, dt_in)
-position = state[:, 0]
-velocity = state[:, 1]
 
-plt.plot(t, position)
+# Plotting Problem 1
+plt.plot(t, state[:, 0])  # first column of state is position
 plt.xlabel('Position')
 plt.ylabel('Time')
 plt.title('Spring-Damper System with MassSpringDamper ODE Model')
 plt.grid()
 plt.savefig('Problem1.png')
 plt.show()
+
+
+""" Initial values of system for Problem 2: Histogram of the Gachapan """
